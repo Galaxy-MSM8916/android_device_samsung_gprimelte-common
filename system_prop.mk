78e87fb29c2946e154bc64222d9436602a43d509
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-04-14 11:36:08 -0400

    system_prop: Add lte radio props

--- system_prop.mk
+++ system_prop.mk
@@ -1,8 +1,10 @@
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
+	persist.radio.snapshot_enabled=1 \
+	persist.radio.snapshot_timer=22 \
+	persist.radio.lte_vrte_ltd=1 \
 	persist.radio.jbims=1 \
 	persist.radio.no_wait_for_card=1
-
 
 # Telephony
 PRODUCT_PROPERTY_OVERRIDES += \
