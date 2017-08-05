75df09c693c128b6d4be68769d1027e316b615d4
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-08-05 13:25:19 -0400

    gprimelte: Decommonise nfc configs and firmware
    Change-Id: Ie7ffab61b21d96bc07ff1f2299162bca6af8adb8

--- product/nfc.mk
+++ product/nfc.mk
@@ -10,15 +10,5 @@ PRODUCT_PACKAGES += \
 	com.android.nfc_extras \
 	libnfc_nci \
 	libnfc_nci_jni \
-	libpn547_fw \
 	NfcNci \
 	Tag
-
-# NFC configs
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml \
-	$(LOCAL_PATH)/configs/nfc/route.xml:system/etc/param/route.xml \
-	$(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-	$(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
-	$(LOCAL_PATH)/configs/nfc/libnfc-sec-hal.conf:system/etc/libnfc-sec-hal.conf \
-	$(LOCAL_PATH)/configs/nfc/libnfc-sec.conf:system/etc/libnfc-sec.conf
