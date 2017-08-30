a16c6408e676e0276cf66adea51934864793acf5
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-08-29 21:18:23 -0400

    gprimelte-common: Remove RemovePackages
     * This never worked at all.
    
    Change-Id: Ibe994c2725e803e2ef871375c9e1eae19ee9b736

--- Android.mk
+++ Android.mk
@@ -4,21 +4,4 @@ ifneq ($(filter fortuna3g fortunave3g fortunalte fortunalteub gprimelte gprimelt
 
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := RemovePackages
-LOCAL_MODULE_TAGS := optional
-LOCAL_CERTIFICATE := shared
-LOCAL_MODULE_CLASS := FAKE
-# It overrrides packages to avoid their addition to the ROM
-LOCAL_OVERRIDES_PACKAGES := \
-	libsecril-client \
-	librilutils \
-	libreference-ril \
-	libril \
-	libtinyalsa \
-	libaudioroute \
-	rild
-
-include $(BUILD_PHONY_PACKAGE)
-
 endif
