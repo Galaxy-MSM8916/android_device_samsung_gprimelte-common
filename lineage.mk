11f4a067595b9bfb9b50d4c941ea84cee58bef4b
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-09-03 02:28:12 -0400

    gprimelte-common: CM -> Lineage
    Change-Id: I8b4a772132051e1a841e8107f064285e10276414

--- lineage.mk
+++ lineage.mk
@@ -5,8 +5,8 @@ PRODUCT_RELEASE_NAME := Samsung Galaxy Grand Prime
 TARGET_SCREEN_WIDTH := 540
 TARGET_SCREEN_HEIGHT := 960
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
