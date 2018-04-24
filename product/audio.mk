f5b327dcfa30aaaf1d227d87588423a5fa76b14f
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-04-24 12:18:47 -0400

    gprimelte-common: Add audio_platform_info config
    Change-Id: I2ff804827c49b3a86948cd7df58ca1a94d57e07a

--- product/audio.mk
+++ product/audio.mk
@@ -2,6 +2,10 @@
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Platform info config
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml
+
 # Permissions
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml
